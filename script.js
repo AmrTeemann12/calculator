4050159c5450ee927c5a156cf4d634e3857d7003
amr_khaled <160667190+AmrTeemann12@users.noreply.github.com>
2026-02-25 03:14:14 +0200

    add new feature and handle operating over previous operation result added pi, convert, and clear last entry buttons. optimized the logic for handling the result of the previous operate function after pressing the "equal" button and storing the returned value at the new "num1" using the variable "overPrevious" with a boolean value for handling specifications.

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function multiply(a, b){
 }
 
 function divide(a, b){
+    if (+b === 0) return "Error";
     return a / b;
 }
 
@@ -19,27 +20,35 @@ let operator = "";
 let num2 = "";
 
 function operate(num1, operator, num2){
+    let result
     switch(operator){
         case "+":
-            return add(num1, num2)
+            result = add(num1, num2)
         break;
         case "-":
-            return subtract(num1, num2)
+            result = subtract(num1, num2)
         break;
         case "×":
-            return multiply(num1, num2)
+            result = multiply(num1, num2)
         break;
         case "÷":
-            return divide(num1, num2)
+            result = divide(num1, num2)
         break;
         default:
-            return num1;
+            result = num1;
     }
+    if (Number.isNaN(result)) return "Error";
+    return result;
 }
 
 function updateOut (){
     const result = operate(+num1, operator, +num2);
+    if(result === "Error") return;
     displayOut.textContent = result;
+}
+
+function updateIn(){
+    displayIn.textContent = `${num1} ${operator} ${num2}`
 }
 
 const numBtns = document.querySelectorAll(".num-btn")
@@ -48,32 +57,64 @@ const equal = document.querySelector("#equal")
 const clrAll = document.querySelector("#clr-all")
 const displayIn = document.querySelector("#input")
 const displayOut = document.querySelector("#output")
+const pi = document.querySelector("#pi")
+const convert = document.querySelector("#convert")
+const clrLast = document.querySelector("#clr-last")
+let overPrevious = false;
 
 numBtns.forEach(btn => btn.addEventListener("click", (e) => {
+    if (overPrevious && !operator){
+        num1 = "";
+        overPrevious = false;
+    }
+
     if (!operator){
-        num1 += e.target.textContent
-    } else {
+        if(!(num1.includes(".") && e.target.textContent === ".")){
+            num1 += e.target.textContent
+        }
+    } else if(!(num2.includes(".") && e.target.textContent === ".")){
         num2 += e.target.textContent
     }
-    displayIn.textContent = `${num1} ${operator} ${num2}`
+
+    updateIn()
     updateOut()
 }))
+
+pi.addEventListener("click", () => {
+    if(overPrevious && !operator){
+        num1 =  "";
+        overPrevious = false;
+    }
+
+    if (!operator && num1 === ""){
+        num1 += Math.PI
+    } else if(!operator && num1){
+        num1 = +num1 * Math.PI
+    } else if(num2 === ""){
+        num2 += Math.PI
+    } else {
+        num2 = +num2 * Math.PI
+    }
+
+    updateIn()
+    updateOut()
+})
 
 opSign.forEach(btn => btn.addEventListener("click", (e) => {
     if (!operator && num1){
         operator = e.target.textContent
     }
-    displayIn.textContent = `${num1} ${operator} ${num2}`
+    updateIn()
 }))
 
 equal.addEventListener("click", () => {
     const result = operate(+num1, operator, +num2)
-    console.log(result)
     displayIn.textContent = result;
     num1 = result;
     operator = "";
     num2 = "";
     displayOut.textContent = "";
+    overPrevious = true;
 })
 
 clrAll.addEventListener("click", () => {
@@ -82,4 +123,34 @@ clrAll.addEventListener("click", () => {
     operator = "";
     displayIn.textContent = "";
     displayOut.textContent = "";
+})
+
+convert.addEventListener("click", () => {
+    if(!operator && num1 && !`${num1}`.includes("-")){
+        num1 = "-" + num1;
+    } else if(!operator && num1 && `${num1}`.includes("-")){
+        num1 = `${num1}`.slice(1);
+    } else if (num2 && !num2.includes("-")){
+        num2 = "-" + num2;
+    } else if(num2 && num2.includes("-")){
+        num2 = num2.slice(1);
+    }
+
+    updateIn()
+    updateOut()
+})
+
+clrLast.addEventListener("click", () => {
+    if(!overPrevious && !operator){
+        num1 = num1.slice(0, -1);
+        if(num1 === "-") num1 = "";
+    } else if(operator && !num2){
+        operator = ""
+    } else if(num2){
+        num2 = num2.slice(0, -1);
+        if(num2 === "-") num2 = "";
+    }
+
+    updateIn()
+    updateOut()
 })
