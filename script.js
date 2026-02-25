ad12dbafc588f8cb092c55e06af22a3832c7e359
amr_khaled <160667190+AmrTeemann12@users.noreply.github.com>
2026-02-25 07:31:10 +0200

    allow operator chaining & optimize π handling refined the equal event handler to calc function and applied it for operators when chained (which safes multi-pressing "equal"). optimized π handling through adding a specialized converting logic for the operate function when the numbers contain "π". added "Error" case exception for the convert sign button to prevent its action.

--- script.js
+++ script.js
@@ -21,6 +21,30 @@ let num2 = "";
 
 function operate(num1, operator, num2){
     let result
+    if(`${num1}`.includes("π")){
+        let split1 = num1.split("π")
+
+        for(let i = 0; i < split1.length; i++){
+            if(split1[i] === "") split1[i] = 1;
+        }
+        let red1 = split1.reduce((sum, item) => sum * item, 1)
+        
+        num1 = red1 * Math.PI**(split1.length-1)
+    }
+
+    if(num2.includes("π")){
+        let split2 = num2.split("π")
+
+        for(let i = 0; i < split2.length; i++){
+            if(split2[i] === "") split2[i] = 1;
+        }
+        let red2 = split2.reduce((sum, item) => sum * item, 1)
+
+        num2 = red2 * Math.PI**(split2.length-1)
+    }
+
+    num1 = +num1;
+    num2 = +num2;
     switch(operator){
         case "+":
             result = add(num1, num2)
@@ -42,7 +66,7 @@ function operate(num1, operator, num2){
 }
 
 function updateOut (){
-    const result = operate(+num1, operator, +num2);
+    const result = operate(num1, operator, num2);
     if(result === "Error") return;
     displayOut.textContent = result;
 }
@@ -51,8 +75,18 @@ function updateIn(){
     displayIn.textContent = `${num1} ${operator} ${num2}`
 }
 
+function calc(){
+    const result = operate(num1, operator, num2)
+    displayIn.textContent = result;
+    num1 = result;
+    operator = "";
+    num2 = "";
+    displayOut.textContent = "";
+    overPrevious = true;
+}
+
 const numBtns = document.querySelectorAll(".num-btn")
-const opSign = document.querySelectorAll(".calc-btn")
+const calcBtns = document.querySelectorAll(".calc-btn")
 const equal = document.querySelector("#equal")
 const clrAll = document.querySelector("#clr-all")
 const displayIn = document.querySelector("#input")
@@ -80,42 +114,19 @@ numBtns.forEach(btn => btn.addEventListener("click", (e) => {
     updateOut()
 }))
 
-pi.addEventListener("click", () => {
-    if(overPrevious && !operator){
-        num1 =  "";
-        overPrevious = false;
-    }
-
-    if (!operator && num1 === ""){
-        num1 += Math.PI
-    } else if(!operator && num1){
-        num1 = +num1 * Math.PI
-    } else if(num2 === ""){
-        num2 += Math.PI
-    } else {
-        num2 = +num2 * Math.PI
-    }
-
-    updateIn()
-    updateOut()
-})
-
-opSign.forEach(btn => btn.addEventListener("click", (e) => {
+calcBtns.forEach(btn => btn.addEventListener("click", (e) => {
     if (!operator && num1){
-        operator = e.target.textContent
+        operator = e.target.textContent;
+        updateIn()
     }
-    updateIn()
+    if(num2){
+       calc()
+       operator = e.target.textContent;
+       updateIn()
+    }
 }))
 
-equal.addEventListener("click", () => {
-    const result = operate(+num1, operator, +num2)
-    displayIn.textContent = result;
-    num1 = result;
-    operator = "";
-    num2 = "";
-    displayOut.textContent = "";
-    overPrevious = true;
-})
+equal.addEventListener("click", calc)
 
 clrAll.addEventListener("click", () => {
     num1 = "";
@@ -126,16 +137,17 @@ clrAll.addEventListener("click", () => {
 })
 
 convert.addEventListener("click", () => {
-    if(!operator && num1 && !`${num1}`.includes("-")){
-        num1 = "-" + num1;
-    } else if(!operator && num1 && `${num1}`.includes("-")){
-        num1 = `${num1}`.slice(1);
-    } else if (num2 && !num2.includes("-")){
-        num2 = "-" + num2;
-    } else if(num2 && num2.includes("-")){
-        num2 = num2.slice(1);
+    if(num1 !== "Error"){ 
+        if(!operator && num1 && !`${num1}`.includes("-")){
+            num1 = "-" + num1;
+        } else if(!operator && num1 && `${num1}`.includes("-")){
+            num1 = `${num1}`.slice(1);
+        } else if (num2 && !num2.includes("-")){
+            num2 = "-" + num2;
+        } else if(num2 && num2.includes("-")){
+            num2 = num2.slice(1);
+        }
     }
-
     updateIn()
     updateOut()
 })
